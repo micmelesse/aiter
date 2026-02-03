0109b8eb54b264e99f4585bd44e23cb349cab563
ShaoChunLee <Shao-Chun.Lee@amd.com>
2026-02-03 19:12:41 +0000

    add test gluon kernel

--- aiter/ops/triton/_triton_kernels/attention/unified_attention.py
+++ aiter/ops/triton/_triton_kernels/attention/unified_attention.py
@@ -688,6 +688,327 @@ def kernel_unified_attention_3d(
     tl.store(segm_max_ptr + segm_offset, M, mask=query_mask_0 & query_mask_1)
     tl.store(segm_expsum_ptr + segm_offset, L, mask=query_mask_0 & query_mask_1)
 
+from triton.experimental import gluon
+import triton.experimental.gluon.language as ttgl
+
+@gluon.jit
+def kernel_unified_attention_3d_gluon(
+    tmp_output_ptr,
+    tmp_stride_0,
+    tmp_stride_1,
+    tmp_stride_2,
+    segm_output_ptr,
+    # [num_tokens, num_query_heads, num_segments, head_size]
+    segm_max_ptr,  # [num_tokens, num_query_heads, num_segments]
+    segm_expsum_ptr,  # [num_tokens, num_query_heads, num_segments]
+    query_ptr,  # [num_tokens, num_query_heads, head_size]
+    key_cache_ptr,  # [num_blks, blk_size, num_kv_heads, head_size]
+    value_cache_ptr,  # [num_blks, blk_size, num_kv_heads, head_size]
+    sink_ptr,  # [num_query_heads]
+    block_tables_ptr,  # [num_seqs, max_num_blocks_per_seq]
+    seq_lens_ptr,  # [num_seqs]
+    alibi_slopes_ptr,  # [num_query_heads]
+    qq_bias_ptr,  # [num_query_tokens, num_query_tokens]
+    scale,  # float32
+    k_scale,  # float32
+    v_scale,  # float32
+    softcap,  # float32
+    num_query_heads: ttgl.constexpr,  # int
+    num_queries_per_kv: ttgl.constexpr,  # int
+    block_table_stride: ttgl.int64,  # int
+    query_stride_0: ttgl.int64,  # int
+    query_stride_1: ttgl.int64,  # int, should be equal to head_size
+    qq_bias_stride_0: ttgl.int64,  # int
+    BLOCK_SIZE: ttgl.constexpr,  # int
+    TILE_SIZE: ttgl.constexpr,  # int, must be power of 2
+    HEAD_SIZE: ttgl.constexpr,  # int
+    HEAD_SIZE_PADDED: ttgl.constexpr,  # int, must be power of 2
+    USE_ALIBI_SLOPES: ttgl.constexpr,  # bool
+    USE_QQ_BIAS: ttgl.constexpr,  # bool
+    USE_SOFTCAP: ttgl.constexpr,  # bool
+    USE_SINKS: ttgl.constexpr,  # bool
+    SLIDING_WINDOW: ttgl.constexpr,  # int
+    stride_k_cache_0: ttgl.int64,  # int
+    stride_k_cache_1: ttgl.int64,  # int
+    stride_k_cache_2: ttgl.int64,  # int
+    stride_k_cache_3: ttgl.constexpr,  # int
+    stride_v_cache_0: ttgl.int64,  # int
+    stride_v_cache_1: ttgl.int64,  # int
+    stride_v_cache_2: ttgl.int64,  # int
+    stride_v_cache_3: ttgl.constexpr,  # int
+    query_start_len_ptr,  # [num_seqs+1]
+    BLOCK_Q: ttgl.constexpr,  # int
+    num_seqs: ttgl.int32,
+    BLOCK_M: ttgl.constexpr,  # int
+    NUM_SEGMENTS_PER_SEQ: ttgl.constexpr,  # int
+    ALL_DECODE: ttgl.constexpr = False,  # bool
+):
+    q_block_global_idx = ttgl.program_id(0)
+    kv_head_idx = ttgl.program_id(1)
+    segm_idx = ttgl.program_id(2)
+
+    # needed to use exp2 (exp2 -> exp conversion)
+    RCP_LN2 = 1.4426950408889634
+    qk_scale = scale * RCP_LN2
+
+    seq_idx = find_seq_idx(
+        query_start_len_ptr, q_block_global_idx, num_seqs, BLOCK_Q, True
+    )
+
+    q_block_start_idx = ttgl.load(query_start_len_ptr + seq_idx) // BLOCK_Q + seq_idx
+
+    q_block_local_idx = q_block_global_idx - q_block_start_idx
+
+    cur_batch_in_all_start_index = ttgl.load(query_start_len_ptr + seq_idx)
+    cur_batch_in_all_stop_index = ttgl.load(query_start_len_ptr + seq_idx + 1)
+
+    cur_batch_query_len = cur_batch_in_all_stop_index - cur_batch_in_all_start_index
+
+    if q_block_local_idx * BLOCK_Q >= cur_batch_query_len:
+        return
+
+    # sequence len for this particular sequence
+    seq_len = ttgl.load(seq_lens_ptr + seq_idx)
+
+    # number of segments for this particular sequence
+    num_segments = NUM_SEGMENTS_PER_SEQ
+    tiles_per_segment = cdiv_fn(seq_len, num_segments * TILE_SIZE)
+
+    if segm_idx * tiles_per_segment * TILE_SIZE >= seq_len:
+        return
+
+    offs_m = ttgl.arange(0, BLOCK_M)
+    offs_d = ttgl.arange(0, HEAD_SIZE_PADDED)
+    offs_t = ttgl.arange(0, TILE_SIZE)
+    query_pos = q_block_local_idx * BLOCK_Q + offs_m // num_queries_per_kv
+    
+    query_offset_0 = cur_batch_in_all_start_index + query_pos
+    query_offset_1 = kv_head_idx * num_queries_per_kv + offs_m % num_queries_per_kv
+    query_offset = (
+        query_offset_0[:, None] * query_stride_0
+        + query_offset_1[:, None] * query_stride_1
+        + offs_d[None, :]
+    )
+
+    if HEAD_SIZE_PADDED != HEAD_SIZE:
+        dim_mask = offs_d < HEAD_SIZE
+    else:
+        dim_mask = ttgl.full((1,), 1, dtype=tl.int1)
+    query_mask_0 = query_pos < cur_batch_query_len
+    query_mask_1 = query_offset_1 < num_query_heads
+
+    # Q : (BLOCK_M, HEAD_SIZE_PADDED)
+    Q = ttgl.load(
+        query_ptr + query_offset,
+        mask=dim_mask[None, :] & query_mask_0[:, None] & query_mask_1[:, None],
+        other=0.0,
+    )
+
+    ttgl.store(
+        tmp_output_ptr + q_block_global_idx * tmp_stride_0 + kv_head_idx * tmp_stride_1 + segm_idx * tmp_stride_2,
+        q_block_local_idx.to(ttgl.int32))
+
+    block_table_offset = seq_idx * block_table_stride
+
+    if USE_SINKS:
+        if segm_idx == 0:
+            # Prescale with RCP_LN2, needed for exp2
+            M = (
+                ttgl.load(
+                    sink_ptr + query_offset_1,
+                    mask=query_mask_1,
+                    other=float("-inf"),
+                ).to(dtype=ttgl.float32)
+                * RCP_LN2
+            )
+        else:
+            M = ttgl.full([BLOCK_M], float("-inf"), dtype=tl.float32)
+    else:
+        M = ttgl.full([BLOCK_M], float("-inf"), dtype=tl.float32)
+
+    L = ttgl.full([BLOCK_M], 1.0, dtype=tl.float32)
+    acc = ttgl.zeros([BLOCK_M, HEAD_SIZE_PADDED], dtype=tl.float32)
+
+    # # context length for this particular sequences
+    # context_len = seq_len - cur_batch_query_len
+
+    # # alibi slope for this head
+    # if USE_ALIBI_SLOPES:
+    #     alibi_slope = tl.load(
+    #         alibi_slopes_ptr + query_offset_1, mask=query_mask_1, other=0.0
+    #     )
+
+    # # query-query attention bias
+    # if USE_QQ_BIAS:
+    #     qq_bias_row_ptrs = (
+    #         qq_bias_ptr + query_pos[:, None] * qq_bias_stride_0
+    #     )  # shape: [BLOCK_M]
+
+    # # compute the length of the longest sequence prefix spanned by any
+    # # query token in the current q_block (q_block_local_idx)
+    # max_seq_prefix_len = (
+    #     context_len
+    #     + q_block_local_idx * BLOCK_Q
+    #     + (BLOCK_M - 1) // num_queries_per_kv
+    #     + 1
+    # )
+
+    # # adjust for potential padding in the last q_block by considering the
+    # # actual sequence length
+    # max_seq_prefix_len = tl.minimum(max_seq_prefix_len, seq_len)
+
+    # # calculate the number of tiles that need to be processed to
+    # # cover the longest sequence prefix (due to causal masking, tiles beyond
+    # # this prefix can be skipped)
+    # num_tiles = cdiv_fn(max_seq_prefix_len, TILE_SIZE)
+
+    # KV_cache_modifier: tl.constexpr = ".cg" if ALL_DECODE else ""
+    # # iterate through tiles within current segment
+    # for j in range(
+    #     segm_idx * tiles_per_segment,
+    #     min((segm_idx + 1) * tiles_per_segment, num_tiles),
+    # ):
+    #     seq_offset = j * TILE_SIZE + offs_t
+    #     if TILE_SIZE == BLOCK_SIZE:
+    #         tile_mask = tl.full((1,), 1, dtype=tl.int1)
+    #     else:
+    #         tile_mask = seq_offset < max_seq_prefix_len
+
+    #     physical_block_idx = tl.load(
+    #         block_tables_ptr + block_table_offset + seq_offset // BLOCK_SIZE
+    #     ).to(tl.int64)
+
+    #     v_offset = (
+    #         physical_block_idx[:, None] * stride_v_cache_0
+    #         + kv_head_idx * stride_v_cache_2
+    #         + offs_d[None, :] * stride_v_cache_3
+    #         + (seq_offset % BLOCK_SIZE)[:, None] * stride_v_cache_1
+    #     )
+
+    #     k_offset = (
+    #         physical_block_idx[None, :] * stride_k_cache_0
+    #         + kv_head_idx * stride_k_cache_2
+    #         + offs_d[:, None] * stride_k_cache_3
+    #         + (seq_offset % BLOCK_SIZE)[None, :] * stride_k_cache_1
+    #     )
+
+    #     # K : (HEAD_SIZE, TILE_SIZE)
+    #     K_load = tl.load(
+    #         key_cache_ptr + k_offset,
+    #         mask=dim_mask[:, None] & tile_mask[None, :],
+    #         other=0.0,
+    #         cache_modifier=KV_cache_modifier,
+    #     )
+
+    #     if K_load.dtype.is_fp8():
+    #         if Q.dtype.is_fp8():
+    #             K = K_load
+    #         else:
+    #             K = (K_load.to(tl.float32) * tl.load(k_scale)).to(Q.dtype)
+    #     else:
+    #         K = K_load
+
+    #     # V : (TILE_SIZE, HEAD_SIZE)
+    #     V_load = tl.load(
+    #         value_cache_ptr + v_offset,
+    #         mask=dim_mask[None, :] & tile_mask[:, None],
+    #         other=0.0,
+    #         cache_modifier=KV_cache_modifier,
+    #     )
+
+    #     if V_load.dtype.is_fp8():
+    #         if Q.dtype.is_fp8():
+    #             V = V_load
+    #         else:
+    #             V = (V_load.to(tl.float32) * tl.load(v_scale)).to(Q.dtype)
+    #     else:
+    #         V = V_load
+
+    #     seq_mask = seq_offset[None, :] < context_len + query_pos[:, None] + 1
+
+    #     # S : (BLOCK_M, TILE_SIZE)
+    #     # qk_scale = scale * RCP_LN2 (log_2 e) so that we can use exp2 later
+    #     S = qk_scale * tl.dot(Q, K)
+
+    #     if USE_SOFTCAP:
+    #         # softcap here uses exp2 and consumes RCP_LN2 conversion.
+    #         # multiply by RCP_LN2 again to be used in later exp2
+    #         S = apply_softcap(S, softcap) * RCP_LN2
+
+    #     S = tl.where(
+    #         query_mask_1[:, None] & query_mask_0[:, None] & seq_mask, S, float("-inf")
+    #     )
+
+    #     if SLIDING_WINDOW > 0:
+    #         S = tl.where(
+    #             (context_len + query_pos[:, None] - seq_offset) < SLIDING_WINDOW,
+    #             S,
+    #             float("-inf"),
+    #         )
+
+    #     if USE_ALIBI_SLOPES:
+    #         # prescale w. RCP_LN2 for later exp2
+    #         S += alibi_slope[:, None] * (seq_offset - context_len) * RCP_LN2
+
+    #     if USE_QQ_BIAS:
+    #         # compute key positions relative to query section
+    #         key_rel_pos = seq_offset - context_len  # shape: [BLOCK_SIZE]
+    #         # load bias only for keys that correspond to queries
+    #         is_query_key = key_rel_pos >= 0 and key_rel_pos < qq_bias_stride_0
+    #         qq_bias = tl.load(
+    #             qq_bias_row_ptrs + key_rel_pos[None, :],
+    #             mask=is_query_key[None, :],  # avoid OOB for context keys
+    #             other=0.0,
+    #         )
+    #         # prescale w. RCP_LN2 for later exp2
+    #         S += qq_bias * RCP_LN2
+
+    #     # compute running maximum
+    #     # m_j : (BLOCK_M,)
+    #     m_j = tl.maximum(M, tl.max(S, axis=1))
+
+    #     # For sliding window there's a chance the max is -inf due to masking of
+    #     # the entire row. In this case we need to set m_j 0 to avoid NaN
+    #     m_j = tl.where(m_j > float("-inf"), m_j, 0.0)
+
+    #     # P : (BLOCK_M, TILE_SIZE,)
+    #     P = tl.math.exp2(S - m_j[:, None])
+
+    #     # l_j : (BLOCK_M,)
+    #     l_j = tl.sum(P, axis=1)
+
+    #     # alpha : (BLOCK_M, )
+    #     alpha = tl.math.exp2(M - m_j)
+
+    #     # acc : (BLOCK_M, HEAD_SIZE_PADDED)
+    #     acc = acc * alpha[:, None]
+
+    #     # update constants
+    #     L = L * alpha + l_j
+    #     M = m_j
+
+    #     # acc : (BLOCK_M, HEAD_SIZE_PADDED)
+    #     acc += tl.dot(P.to(V.dtype), V)
+
+    # segm_output_offset = (
+    #     query_offset_0[:, None].to(tl.int64)
+    #     * (num_query_heads * NUM_SEGMENTS_PER_SEQ * HEAD_SIZE_PADDED)
+    #     + query_offset_1[:, None] * (NUM_SEGMENTS_PER_SEQ * HEAD_SIZE_PADDED)
+    #     + segm_idx * HEAD_SIZE_PADDED
+    #     + tl.arange(0, HEAD_SIZE_PADDED)[None, :]
+    # )
+    # tl.store(
+    #     segm_output_ptr + segm_output_offset,
+    #     acc,
+    #     mask=dim_mask[None, :] & query_mask_0[:, None] & query_mask_1[:, None],
+    # )
+    # segm_offset = (
+    #     query_offset_0.to(tl.int64) * (num_query_heads * NUM_SEGMENTS_PER_SEQ)
+    #     + query_offset_1 * NUM_SEGMENTS_PER_SEQ
+    #     + segm_idx
+    # )
+    # tl.store(segm_max_ptr + segm_offset, M, mask=query_mask_0 & query_mask_1)
+    # tl.store(segm_expsum_ptr + segm_offset, L, mask=query_mask_0 & query_mask_1)
 
 @triton.jit
 def reduce_segments(
